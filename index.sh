9c80567cfa7a2bba9d94e11c13e444cdae74797d
smallporridge <32667408+smallporridge@users.noreply.github.com>
2024-11-11 14:56:34 +0800

    Update index.sh

--- index.sh
+++ index.sh
@@ -1,7 +1,7 @@
 python -m pyserini.index.lucene \
   --collection JsonCollection \
   --input dataset/hotpot/corpus \
-  --index /data/webgpt/IIA/dataset/hotpot/index \
+  --index /AssistRAG/dataset/hotpot/index \
   --generator DefaultLuceneDocumentGenerator \
   --threads 1 \
   --storePositions --storeDocvectors --storeRaw
